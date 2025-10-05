bf2b6bce19811527a8104842f6318371fc519b22
Américo González <agzz2003@gmail.com>
2025-10-05 06:12:46 -0600

    ajustes en certificados

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
             'ssl': {
                 # Puedes usar 'require' si tienes el certificado
                 #'ssl_mode': 'REQUIRED',
-                'ssl_mode': 'DISABLED',
+                'ssl': {'ssl_mode': 'DISABLED'},
                 'ca': '../certs/azure_mysql_chain.pem',  # Ruta al certificado CA si es necesario
             },
             # Configuración de modo SQL estricto, generalmente recomendada para MySQL
