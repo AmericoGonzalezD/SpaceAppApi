e108e5f570ed07d1afd62b6cc2889d0696d06bd5
Américo González <agzz2003@gmail.com>
2025-10-05 07:38:39 -0600

    ajuste de direccion de carpeta stticfiles

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_URL = '/static/' 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
